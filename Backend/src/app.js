bcac01341eafc03a4449e73c161acfa12ea9b73d
HassanThePassionate <maqsoodia777@gmail.com>
2025-02-12 14:04:38 +0500

    some Changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,7 +3,11 @@ const aiRoutes = require("./routes/ai.routes");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://code-reviewer-zk3p.vercel.app",
+  })
+);
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
